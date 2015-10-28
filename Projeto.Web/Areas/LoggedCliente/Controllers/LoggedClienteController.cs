f55d888a4b30525e340f87bf396c1b10b3f86919
taysontey <taysontey@gmail.com>
2015-10-28 11:14:23 -0200

    Logout Funcionario/Cliente

--- Projeto.Web/Areas/LoggedCliente/Controllers/LoggedClienteController.cs
+++ Projeto.Web/Areas/LoggedCliente/Controllers/LoggedClienteController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Projeto.Web.Models.Usuario;
 
 namespace Projeto.Web.Areas.LoggedCliente.Controllers
 {
     public class LoggedClienteController : Controller
     {
-        // GET: LoggedCliente/LoggedCliente
-        public ActionResult Index()
+        [Authorize]
+        public ActionResult Logout()
         {
-            return View();
+            FormsAuthentication.SignOut();
+
+            Session.Remove("clientelogado");
+            Session.Abandon();
+
+            return View("../Usuario/Login");
         }
     }
 }
--- Projeto.Web/Areas/LoggedFuncionario/Controllers/LoggedFuncionarioController.cs
+++ Projeto.Web/Areas/LoggedFuncionario/Controllers/LoggedFuncionarioController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Projeto.Web.Areas.LoggedFuncionario.Controllers
 {
     public class LoggedFuncionarioController : Controller
     {
-        // GET: LoggedFuncionario/LoggedFuncionario
-        public ActionResult Index()
+        [Authorize]
+        public ActionResult Logout()
         {
-            return View();
+            FormsAuthentication.SignOut();
+
+            Session.Remove("funcionariologado");
+            Session.Abandon();
+
+            return View("../Usuario/Login");
         }
     }
 }

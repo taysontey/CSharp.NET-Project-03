8c8886ca4b387e5ae4fbbc596cf993e72f698de8
taysontey <taysontey@gmail.com>
2015-11-06 14:05:53 -0200

    Updated List  + Edit Chamados (Funcionario)

--- Projeto.Web/Areas/LoggedFuncionario/Controllers/LoggedFuncionarioController.cs
+++ Projeto.Web/Areas/LoggedFuncionario/Controllers/LoggedFuncionarioController.cs
@@ -26,6 +26,12 @@ namespace Projeto.Web.Areas.LoggedFuncionario.Controllers
         }
 
         [Authorize]
+        public ActionResult ListaChamado()
+        {
+            return View();
+        }
+
+        [Authorize]
         public ActionResult NovaSenha()
         {
             return View();
@@ -33,7 +39,90 @@ namespace Projeto.Web.Areas.LoggedFuncionario.Controllers
 
         #region Metodos AJAX
 
-        
+        public JsonResult EdicaoChamado(ChamadoModelEdicao model)
+        {
+            try
+            {
+                Funcionario f = (Funcionario)Session["funcionariologado"];
+
+                ChamadoDal d = new ChamadoDal();
+
+                Chamado chamado = d.FindById(model.IdChamado);
+
+                Chamado ch = new Chamado();
+
+                ch.IdChamado = chamado.IdChamado;
+                ch.Assunto = chamado.Assunto;
+                ch.Descricao = chamado.Descricao;
+                ch.Solucao = chamado.Solucao;
+
+                return Json(ch, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+        }
+
+        public JsonResult EditarChamado(ChamadoModelEdicao model)
+        {
+            try
+            {
+                Funcionario f = (Funcionario)Session["funcionariologado"];
+
+                ChamadoDal d = new ChamadoDal();
+
+                Chamado chamado = d.FindById(model.IdChamado);
+
+                chamado.Solucao = model.Solucao;
+                chamado.Situacao = "Fechado";
+                chamado.DataFechamento = DateTime.Now;
+                chamado.Funcionario = f;
+
+                d.SaveOrUpdate(chamado);
+
+                return Json("Chamado Atualizado.");
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+        }
+
+        public JsonResult ListarChamado()
+        {
+            try
+            {
+                Funcionario f = (Funcionario)Session["funcionariologado"];
+
+                ChamadoDal d = new ChamadoDal();
+
+                List<Chamado> lista = d.FindAll();
+
+                List<Chamado> list = new List<Chamado>();
+
+                foreach(Chamado chamado in lista)
+                {
+                    Chamado ch = new Chamado();
+
+                    ch.IdChamado = chamado.IdChamado;
+                    ch.Assunto = chamado.Assunto;
+                    ch.Situacao = chamado.Situacao;
+                    ch.DataAbertura = chamado.DataAbertura;
+
+                    ch.Cliente = new Cliente();
+                    ch.Cliente.Nome = chamado.Cliente.Nome;
+
+                    list.Add(ch);
+                }
+
+                return Json(list);
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+        }
 
         #endregion
     }

2b2e50026273b90e513112c85f872083c592edd3
taysontey <taysontey@gmail.com>
2015-11-06 23:46:33 -0200

    Missing Files

---
+++ Projeto.Web/Areas/LoggedFuncionario/Models/ChamadoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto.Web.Areas.LoggedFuncionario.Models
+{
+    public class ChamadoModelEdicao
+    {
+        public int IdChamado { get; set; }
+        public string Solucao { get; set; }
+    }
+
+    public class ChamadoModelConsulta
+    {
+        public int IdChamado { get; set; }
+        public string Situacao { get; set; }
+        public DateTime DataInicial { get; set; }
+        public DateTime DataFinal { get; set; }
+    }
+}
